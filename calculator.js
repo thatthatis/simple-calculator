68507616bad8709b267583f112b21391dd6cc934
Mat Beecher <mat.beecher@gmail.com>
2022-07-06 10:24:03 -0700

    initial commit

---
+++ calculator.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express()
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.get("/", function (req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.post("/", function (req, res){
+    var num1 = Number(req.body.num1);
+    var num2 = Number(req.body.num2);
+
+    var result = num1 + num2;
+    
+    console.log(req.body.num1);
+    console.log(req.body.num2);
+    res.send("The answer is " + result);   
+});
+
+app.listen(3000, function(){
+    console.log("Server started on port 3000");
+});
